6d9ec76d5960179cd461237cf3f96968835b872a
Amazo Michaela <vladislovewaves@gmail.com>
2024-02-23 21:43:01 +0300

    edit readme

--- CICD/src/telegram.sh
+++ CICD/src/telegram.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TELEGRAM_BOT_TOKEN="5980992821:AAHa6uijnwOgcusi8qRhU7bJ-c97OC1l5uA"
-TELEGRAM_USER_ID="374436151"
+TELEGRAM_BOT_TOKEN="******************************"
+TELEGRAM_USER_ID="*********"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AStatus:+$CI_JOB_STATUS%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
